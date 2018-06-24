2328a2f713b79975ca72af21e09c00efa19e9dfe
LIU <1163062842@qq.com>
2018-06-24 17:19:15 +0800

    sushe june 24

--- kuaTao/js/index.js
+++ kuaTao/js/index.js
@@ -1,46 +1,74 @@
 ;$(function(){
-	var $dropdown=$('.dropdown');
-	/*
-	$dropdown.on('dropdown-show dropdown-shown dropdown-hide dropdown-hidden',function(ev){
-		console.log(ev.type)
-	});
-	*/
-	$dropdown.on('dropdown-show',function(ev){
-		var $this=$(this);
-		var loadUrl=$this.data('load');//获取需要请求的地址
-		var isLoaded=$this.data('isLoaded');
+	/*下拉菜单*/
+		var $dropdown=$('.dropdown');
+		/*
+		$dropdown.on('dropdown-show dropdown-shown dropdown-hide dropdown-hidden',function(ev){
+			console.log(ev.type)
+		});
+		*/
+		$dropdown.on('dropdown-show',function(ev){
+			var $this=$(this);
+			var loadUrl=$this.data('load');//获取需要请求的地址
+			var isLoaded=$this.data('isLoaded');
 
-		if(!loadUrl)  return;//判断是否取到loadurl，若没有取到，直接返回
-		if(isLoaded)  return;//判断是否已执行过Ajax请求,若已加载过数据，就直接返回
+			if(!loadUrl)  return;//判断是否取到loadurl，若没有取到，直接返回
+			if(isLoaded)  return;//判断是否已执行过Ajax请求,若已加载过数据，就直接返回
 
-		$.getJSON(loadUrl,function(data){
-			var html='';
-			for(var i=0;i<=data.length;i++){
-				html+='<li ><a href="'+data[i].url+'" class="menu-item link">'+data[i].name+'</a></li>'
-			}
-			//模拟网络延时，在crome浏览器测试需要先启动一个server服务
-			
-			setTimeout(function(){
-				$this.find('.dropdown-layer').html(html);
-				$this.data('isLoaded',true)
-			},1000);
+			$.getJSON(loadUrl,function(data){
+				var html='';
+				for(var i=0;i<=data.length;i++){
+					html+='<li ><a href="'+data[i].url+'" class="menu-item link">'+data[i].name+'</a></li>'
+				}
+				//模拟网络延时，在crome浏览器测试需要先启动一个server服务
+				
+				setTimeout(function(){
+					$this.find('.dropdown-layer').html(html);
+					$this.data('isLoaded',true)
+				},1000);
+			})
+		});
+
+		$dropdown.dropdown({
+			css3:true,
+			js:false,
+			mode:'slideUpDown',
+			// eventName:'click',
+			 delay:200   // 划过元素时不会立即出发下拉效果
+		});
+		/*html文件里的button已删掉，这个是留着给另一个index.html做测试*/
+		$('button').eq(0).click(function(){
+			console.log(this)
+			$dropdown.dropdown('show');//运行此行代码需先将dropdown暴露出去，具体见dropdown.js
 		})
-	});
-
-	$dropdown.dropdown({
-		css3:true,
-		js:false,
-		mode:'slideUpDown',
-		// eventName:'click',
-		 delay:200   // 划过元素时不会立即出发下拉效果
-	});
-	/*html文件里的button已删掉，这个是留着给另一个index.html做测试*/
-	$('button').eq(0).click(function(){
-		console.log(this)
-		$dropdown.dropdown('show');//运行此行代码需先将dropdown暴露出去，具体见dropdown.js
-	})
-	$('button').eq(1).click(function(){
-		$dropdown.dropdown('hide');//运行此行代码需先将dropdown暴露出去，具体见dropdown.js
-	})
+		$('button').eq(1).click(function(){
+			$dropdown.dropdown('hide');//运行此行代码需先将dropdown暴露出去，具体见dropdown.js
+		})
+	/*下拉菜单*/
+	/*搜索框*/
+		var $search=$('.search');
+		$search.search({
+			autocomplete:true,
+			css3:true,
+			js:true,
+			mode:'fade',
+			url:'https://suggest.taobao.com/sug?code=utf-8&_ksTS=1529744566102_802&k=1&area=c2c&bucketid=6&q='
+		});
+		$search.on('getdata',function(ev,data,$Layer){
+			if(data.result.length==0){
+				$Layer.html('').hide();//若查询不到相应数据则不执行以下代码，提高效率
+				return;
+			}
+			var html='';
+			var dataNum=5;
+			for(var i=0;i<data.result.length;i++){
+				if(i>dataNum) break;//控制数据的数量
+				html+='<li class="search-item">'+data.result[i][0]+'</li>'
+			}
+			$Layer.html(html).showHide('show');
+		});
+		$search.on('getNoData',function($Layer){
+			$Layer.html('').showHide('hide');
+		})
+	/*搜索框*/
 	
 })
--- kuaTao/js/search.js
+++ kuaTao/js/search.js
@@ -1,61 +1,155 @@
 ;(function($){
-	var $searchForm=$('#search-form'),
-		$searchInput=$('.search-input'),
-		$searchLayer=$('.search-layer');
-	$searchForm.on('submit',function(){
-		if(getInputVal()==''){//若搜索栏内容是否为空或者空格，则不执行提交
-			return false;
+	function Search($elem,options){
+		this.$elem=$elem;
+		this.options=options;
+		this.$searchForm=this.$elem.find('#search-form');
+		this.$searchInput=this.$elem.find('.search-input');
+		this.$searchLayer=this.$elem.find('.search-layer');
+		this.searchBtn=this.$elem.find('.search-btn');
+		
+		this._init();
+
+		if(this.options.autocomplete){
+			this.auto();
 		}
-	});
-
-	//当用户输入时动态获取提示数据
-	var url='https://suggest.taobao.com/sug?code=utf-8&_ksTS=1529744566102_802&callback=jsonp803&k=1&area=c2c&bucketid=6&q='
-	$searchInput.on('input',function(){
-		$.ajax({
-			url:url+getInputVal(),//拼接要请求的地址,q=后边加上输入的内容
-			dataType:'jsonp',
-			jsonp:'callback'
-		})
-		.done(function(data){
-			if(data.result.length==0){
-				$searchLayer.html('').hide();//若查询不到相应数据则不执行以下代码，提高效率
-				return;
-			}
-			var html='';
-			var dataNum=5;
-			for(var i=0;i<data.result.length;i++){
-				if(i>dataNum) break;//控制数据的数量
-				html+='<li class="search-item">'+data.result[i][0]+'</li>'
-			}
-			$searchLayer.html(html).show();
-		})
-		.fail(function(err){
-				$searchLayer.html('').hide();
-		})
-		.always(function(){
-			console.log('always');
-		})
-	});
-
-	$searchLayer.on('click','.search-item',function(){//事件委托
-		$searchInput.val(removeHTMLTag($(this).html()));
-		$searchForm.trigger('submit');
-	});
-	$(document).on('click',function(){
-		$searchLayer.hide();
-	});
-	$searchInput.on('focus',function(){
-		$searchLayer.show();
-	})
-	.on('click',function(ev){
-		ev.stopPropagation()
-	})
-
-
-	function getInputVal(){
-		return $.trim($searchInput.val());
 	}
-	function removeHTMLTag(str){
-		return str.replace(/<[^<|>]+>/g,'')
+	Search.prototype={
+		constructor:Search,
+		_init:function(){
+			//绑定事件
+			this.searchBtn.on('click',$.proxy(this.submit,this));
+		},
+		submit:function(){
+			if(this.getInputVal()==''){
+				return false;
+			}
+			this.$searchForm.trigger('subnit');
+		},
+		getInputVal:function(){
+			return $.trim(this.$searchInput.val());
+		},
+		auto:function(){
+			this.$searchInput
+			.on('input',$.proxy(this.getData,this))
+			.on('focus',$.proxy(this.showLayer,this))
+			.on('click',function(ev){
+				ev.stopPropagation();
+			});
+			$(document).on('click',$.proxy(this.hideLayer,this));
+			this.$searchLayer.showHide($.proxy(this.options,this));
+		},
+		getData:function(){//要使每个函数都单一的做一件事
+			var self=this;
+				$.ajax({
+					url:self.options.url+self.getInputVal(),//拼接要请求的地址,q=后边加上输入的内容
+					dataType:'jsonp',
+					jsonp:'callback'
+				})
+				.done(function(data){//这里传递参数需要用[ ]包裹
+					self.$elem.trigger('getdata',[data,self.$searchLayer]);
+				})
+				.fail(function(err){
+					self.$elem.trigger('getNoData',[self.$searchLayer]);
+				})
+				.always(function(){
+					console.log('always');
+				})
+		},
+		showLayer:function(){
+			if($.trim(this.$searchLayer.html())=='') return;
+			this.$searchLayer.showHide('show');		
+		},
+		hideLayer:function(){
+			this.$searchLayer.showHide('hide');
+		}
 	}
+	Search.DEFAULTS={
+		autocomplete:false,
+		css3:false,
+		js:false,
+		mode:'fade',
+		url:'https://suggest.taobao.com/sug?code=utf-8&_ksTS=1529744566102_802&k=1&area=c2c&bucketid=6&q='
+	}
+	$.fn.extend({
+		search:function(options){
+			return this.each(function(){
+				var $this=$(this);
+				var search=$this.data('.search');
+				if(!search){
+					options=$.extend(Search.DEFAULTS,options);
+					search=new Search($(this),options);
+					$this.data('search',search);
+				}
+				if(typeof search[options]=='function'){
+					search[options]();
+				}
+			})
+		}	
+	})
+	// var $searchForm=$('#search-form'),
+	// 	$searchInput=$('.search-input'),
+	// 	$searchLayer=$('.search-layer');
+	// $searchForm.on('submit',function(){
+	// 	if(getInputVal()==''){//若搜索栏内容是否为空或者空格，则不执行提交
+	// 		return false;
+	// 	}
+	// });
+
+	// //当用户输入时动态获取提示数据
+	// var url='https://suggest.taobao.com/sug?code=utf-8&_ksTS=1529744566102_802&callback=jsonp803&k=1&area=c2c&bucketid=6&q='
+	// $searchInput.on('input',function(){
+	// 	$.ajax({
+	// 		url:url+getInputVal(),//拼接要请求的地址,q=后边加上输入的内容
+	// 		dataType:'jsonp',
+	// 		jsonp:'callback'
+	// 	})
+	// 	.done(function(data){
+	// 		if(data.result.length==0){
+	// 			$searchLayer.html('').hide();//若查询不到相应数据则不执行以下代码，提高效率
+	// 			return;
+	// 		}
+	// 		var html='';
+	// 		var dataNum=5;
+	// 		for(var i=0;i<data.result.length;i++){
+	// 			if(i>dataNum) break;//控制数据的数量
+	// 			html+='<li class="search-item">'+data.result[i][0]+'</li>'
+	// 		}
+	// 		$searchLayer.html(html).show();
+	// 	})
+	// 	.fail(function(err){
+	// 			$searchLayer.html('').hide();
+	// 	})
+	// 	.always(function(){
+	// 		console.log('always');
+	// 	})
+	// });
+
+	// $searchLayer.on('click','.search-item',function(){//事件委托
+	// 	$searchInput.val(removeHTMLTag($(this).html()));
+	// 	$searchForm.trigger('submit');
+	// });
+	// $(document).on('click',function(){
+	// 	$searchLayer.hide();
+	// });
+	// $searchInput.on('focus',function(){
+	// 	if($.trim($searchLayer.html())==''){
+	// 		// return false;
+	// 		$searchLayer.hide();
+	// 	}
+	// 	else{
+	// 		$searchLayer.show();
+	// 	}
+		
+	// })
+	// .on('click',function(ev){
+	// 	ev.stopPropagation()
+	// })
+
+
+	// function getInputVal(){
+	// 	return $.trim($searchInput.val());
+	// }
+	// function removeHTMLTag(str){
+	// 	return str.replace(/<[^<|>]+>/g,'')
+	// }
 })(jQuery)
