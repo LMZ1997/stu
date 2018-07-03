7f8fb1880f3dbd027e8bcee6aafd9ab79c749ebc
LIU <1163062842@qq.com>
2018-07-03 16:57:12 +0800

    new sushe july 3

--- kuaTao/js/index.js
+++ kuaTao/js/index.js
@@ -2,9 +2,11 @@
 	/*下拉菜单*/
 		var $menu=$('.nav-header .dropdown');
 		var htmlMenu='';
+		/*
 		for(var i=0;i<=data.length;i++){
 			htmlMenu+='<li ><a href="'+data[i].url+'" class="menu-item link">'+data[i].name+'</a></li>'
 		}
+		*/
 		var optionsMenu={
 			css3:true,
 			js:false,
@@ -83,13 +85,15 @@
 	/*分类导航开始*/
 		var $category=$('.category .dropdown');
 		var htmlCategory='';
+		/*
 		for(var i=0;i<=data.length;i++){
 			htmlCategory+='<dl class="category-details"><dt class="category-details-title fl"><a href="#" class="category-details-title-link">'+data[i].title+'</a></dt><dd class="category-details-item fl">';
 			for (var j = 0; j < data[i].items.length.length; j++) {
 				htmlCategory+='<a href="#" class="link">'+data[i].items[j]+'</a>'
-			}
+			} 
 			htmlCategory+='</dd></dl>';
 		}
+		*/
 		var optionsCategory={
 			css3:false,
 			js:true,
@@ -97,4 +101,136 @@
 		}
 		createDropdownLayer($category,htmlCategory,optionsCategory)
 	/*分类导航结束*/
+	/*轮播图开始*/
+		lunBo();
+		function lunBo(){
+			function Carousel(option){
+					this.oBox=document.getElementById(option.id);
+					this.oUl=null;
+					this.now=0;
+					this.aImg=option.aImg;
+					this.width=option.width;
+					this.height=option.height;
+					this.oBox.zIndex=9;
+					// this.oLi=null;
+					// this.oImg=null;
+					// this.oLeft=null;
+					// this.oRight=null;
+					this.init();//此处要放在最后，所有声明属性的后边
+					this.bindEvent();
+					this.playDuration=option.playDuration;
+					if(option.playDuration){
+						this.move();
+					}
+			}
+			Carousel.prototype.init=function(){
+					this.oBox.style.width=this.width+'px';
+					this.oBox.style.height=this.height+'px';
+					this.oBox.style.position='relative';
+					this.oUl=document.createElement('ul');
+					this.botUl=document.createElement('ul');
+					this.botUl.className='botUl';
+					for (var i = 0; i < this.aImg.length; i++) {
+						var oLi=document.createElement('li');
+						var botLi=document.createElement('li');
+						var oImg=document.createElement('img');
+						oLi.style.position='absolute';
+						if(i==0){
+							oLi.style.opacity=1;
+							oLi.style.zIndex=50;
+							botLi.className='active';
+						}else{
+							oLi.style.opacity=0.5;
+							oLi.style.zIndex=0;
+							botLi.className='';
+						}			
+						oImg.src=this.aImg[i];
+						oImg.style.width=this.width+'px';
+						oImg.style.height=this.height+'px';			
+						this.oUl.appendChild(oLi);
+						oLi.appendChild(oImg);				
+						this.botUl.appendChild(botLi);			
+					}
+
+					this.oLeft=document.createElement('span');
+					this.oRight=document.createElement('span');
+					// this.oLeft.className='oLeft';
+					// this.oRight.className='oRight';
+					// this.oLeft.innerHTML='&lt;';		
+					// this.oRight.innerHTML='&gt;';
+					// this.oBox.appendChild(this.oLeft);
+					// this.oBox.appendChild(this.oRight);	
+					this.oBox.appendChild(this.botUl);
+					this.oBox.appendChild(this.oUl);
+			}
+			Carousel.prototype.bindEvent=function(){
+					// this.oRight.onclick=function(){
+					// 	this.now++;
+					// 	this.Tab();
+					// }.bind(this);
+					// this.oLeft.onclick=function(){
+					// 	this.now--;
+					// 	this.Tab();
+					// }.bind(this);
+					this.botUl.addEventListener('click',function(ev){
+							var oEvent=ev||event;
+							// this.botUl.children.className='';
+							for(var j=0;j<this.botUl.children.length;j++){
+								this.botUl.children[j].className='';
+							}
+							oEvent.target.className='active';
+							for(k=0;k<this.botUl.children.length;k++){
+								if(this.botUl.children[k].className=='active'){
+									this.now=k;
+									console.log(this.now)
+								}
+							}
+							this.Tab();
+					}.bind(this),false)
+			}
+			Carousel.prototype.Tab=function(){
+					for (var i = 0; i < this.oUl.children.length; i++) {
+							this.oUl.children[i].style.zIndex=0;
+							this.oUl.children[i].style.opacity=0;
+							this.botUl.children[i].className='';
+						}
+						if(this.now>=this.oUl.children.length){
+							this.now=0;
+						}else if(this.now<0){
+							this.now=this.oUl.children.length-1;
+						}
+						this.oUl.children[this.now].style.zIndex=50;
+						// this.oUl.children[this.now].style.opacity=1;
+						animation(this.oUl.children[this.now],{opacity:100});
+						this.botUl.children[this.now].className='active';
+			}
+			Carousel.prototype.move=function(){
+					var Timer=null;
+					// Timer=setInterval(function(){
+					// 	this.now++;
+					// 	if(this.now>=this.oUl.children.length){
+					// 		this.now=0;
+					// 	}
+					// 	console.log(this);
+					// 	this.Tab();
+					// }.bind(this),1000)
+
+					//等同于
+					Timer=setInterval(this.oRight.onclick,this.playDuration);
+					this.oBox.onmouseover=function(){
+						clearInterval(Timer);
+					}
+					this.oBox.onmouseout=function(){
+						Timer=setInterval(this.oRight.onclick,this.playDuration);
+					}.bind(this);
+			}
+			new Carousel({
+				id:'carousel',
+				aImg:['images/lunbo1.jpg','images/lunbo2.jpg','images/lunbo3.jpg','images/lunbo4.jpg'],
+				width:728,
+				height:504,
+				playDuration:2000
+			});	
+		}
+	/*轮播图结束*/
 })
