1ad7b7f2c18832352da29248f67b7ffa0fd06409
LIU <1163062842@qq.com>
2018-08-09 20:40:07 +0800

    sushe Aug9

--- blog/models/article.js
+++ blog/models/article.js
@@ -3,17 +3,28 @@ const mongoose=require('mongoose');
 const roles=new mongoose.Schema({
 		category:{//所属分类
 			type:mongoose.Schema.Types.ObjectId,
-			// default:
+			ref:'Cate'
+		},
+		intro:{
+			type:String
 		},
 		user:{
 			type:mongoose.Schema.Types.ObjectId,
 			ref:'User'
-		}
+		},
 		title:{
 			type:String
 		},
 		content:{
 			type:String
+		},
+		click:{
+			type:Number,
+			default:0
+		},
+		createdAt:{
+			type:Date,
+			default:Date.now
 		}
 
 
--- blog/routes/article.js
+++ blog/routes/article.js
@@ -4,14 +4,26 @@ const CateModel=require('../models/category.js')
 const page=require('../util/page.js')
 const articleModel=require('../models/article.js')
 
-// console.log('aaa')
+
 router.get('/',(req,res)=>{
-	console.log('aaa')
 	res.render('admin/article')
 })
 
 
 router.get('/add',(req,res)=>{
-	res.render('admin/article')
+	CateModel.find({},'_id name')
+	.sort({order:1})
+	.then((categories)=>{
+		res.render('admin/article_add',{
+			userInfo:req.userInfo,
+			categories:categories,
+		})
+	})
+	
+})
+
+router.post('/add',(req,res)=>{//post请求
+	let body=req.body;
+	
 })
 module.exports=router;
