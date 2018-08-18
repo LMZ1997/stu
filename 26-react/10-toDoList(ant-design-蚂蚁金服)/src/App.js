3685a7167c238f81433bf9e1279c09e7b518e78b
LIU <1163062842@qq.com>
2018-08-18 19:15:29 +0800

    sushe Aug18

--- 26-react/10-toDoList(ant-design-蚂蚁金服)/src/App.js
+++ 26-react/10-toDoList(ant-design-蚂蚁金服)/src/App.js
@@ -2,19 +2,18 @@ import React,{Component,Fragment} from 'react';
 
 import './App.css'
 
-import { DatePicker } from 'antd';
+import { DatePicker,Input,Button,Row,Col} from 'antd';
  // import 'antd/dist/antd.css';//太消耗性能，所以尽量用按需加载
 
 class App extends Component{
-
-
-
-
 	render(){ 
-		console.log('App render...')
 		return (
 			<div className='App'>
 				<DatePicker/>
+				<Row>
+			      <Col span={18}><Input/></Col>
+			      <Col span={6}><Button type="primary">添加</Button></Col>
+			    </Row>
 			</div>
 			
 		)
